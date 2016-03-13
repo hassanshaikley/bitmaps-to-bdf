19f13b164e1f17fee8cf97d30c86e971c06c6692
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-03-13 06:05:18 -0700

    Some finishing touches

--- font-gen.cc
+++ font-gen.cc
@@ -1,3 +1,18 @@
+// -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+// Copyright (C) 2016 Hassan Shaikley <hassan.shaikley@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 2.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://gnu.org/licenses/gpl-2.0.txt>
+
 // .bdf font generator
 // generates font data from the bitmaps in flaschen-taschen-font.h
 
@@ -17,13 +32,14 @@ int main(int argc, char *argv[]) {
     }
     write_character_data(f, incr + START_CHAR, incr); 
   }
+
   fprintf(f, "ENDFONT\n");
   fclose(f);
 }
 
 void write_character_data(FILE *f, int ascii, int glyph_number){
-  fprintf(f, "STARTCHAR %c\n", ( ascii));
-  fprintf(f, "ENCODING %d\n", ( ascii));
+  fprintf(f, "STARTCHAR %c\n", (ascii));
+  fprintf(f, "ENCODING %d\n", (ascii));
   fprintf(f, "SWIDTH 540 0\n");
   fprintf(f, "DWIDTH 5 0\n");
   fprintf(f, "BBX 5 5 0 -1\n");
@@ -32,5 +48,6 @@ void write_character_data(FILE *f, int ascii, int glyph_number){
   for (int i = 0; i < 5 ; i ++){
     fprintf(f, "%02x\n", glyph[glyph_number][i] << 3);
   }
+
   fprintf(f, "ENDCHAR\n");
 }
