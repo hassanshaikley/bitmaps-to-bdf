b350be11b1cf1a026988df3cc1314a70165cf108
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-03-13 01:14:26 -0800

    Does everything except for conversion from char bitmap to font hex bit map

--- font-example.cc
+++ font-example.cc
@@ -6,29 +6,47 @@
 #include "flaschen-taschen-font.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <map> 
 #include <string>
 
 #define DISPLAY_WIDTH  40
 #define DISPLAY_HEIGHT 40
 
+// Output file is 5x5.bdf
 int main(int argc, char *argv[]) {
 
   char a = '!';
   uint8_t * exclam = glyph[a - '!'];
 
+  system("cp header.txt 5x5.bdf");
+
+  //FUcking up for ' lol
   for(int incr=0; incr < sizeof(glyph)/sizeof(*glyph);++incr){
+    std::string STARTCHAR =  "STARTCHAR ";
+    STARTCHAR += (incr+ '!');
 
-
-  /*  for(int y=0; y<5; y++){
-      for(int x=0; x<5; x++){
-	if((exclam[y]) & (1<<(4-x)) ){ // cmp glyph val & left shifted 1
-	  canvas.SetPixel(x+5 , y+5, red);        //  fill sample with color variable.
-	  printf("set a red pixel at (%d,%d)\n", x+10, y+10);  
-	} else {
-	  printf("didnt set a red pixel at (%d,%d)\n", x+10, y+10);  
-	}
-      }
-    }*/
+    std::string call = "echo ";
+    call+= STARTCHAR;
+    call+= " >>5x5.bdf"; 
+    system(("echo '"+ STARTCHAR + "' >>5x5.bdf" ).c_str());
+    system(("echo 'ENCODING '"+ std::to_string(incr +'!') +" >>5x5.bdf" ).c_str());
+    system(("echo 'ENCODING '"+ std::to_string(incr +'!') +" >>5x5.bdf" ).c_str());
+    system("echo 'SWIDTH 540 0' >>5x5.bdf" );
+    system("echo 'DWIDTH 5 0' >>5x5.bdf" );
+    system("echo 'DWIDTH BBX 5 5 0 -1' >>5x5.bdf" );
+    system("echo 'BITMAP' >>5x5.bdf" );
+    /*
+       BBX 4 6 0 -1
+       BITMAP
+       00
+       00
+       00
+       00
+       00
+       00
+     */
+    system("echo 'ENDCHAR' >> 5x5.bdf");
   }
+  system("echo 'ENDFONT' >>5x5.bdf" );
 }
