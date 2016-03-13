5f543b3548c4e93737eaf147e72c875491582518
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-03-13 05:30:40 -0700

    Removed unnecessary code

--- font-gen.cc
+++ font-gen.cc
@@ -1,32 +1,18 @@
-// 
-// flaschen taschen string to font decoder 
-// expand w/ info how to write and manipulate a font 
-//
+// .bdf font generator
+// generates font data from the bitmaps in flaschen-taschen-font.h
 
 #include "flaschen-taschen-font.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <map> 
-#include <string>
 
-#define DISPLAY_WIDTH  40
-#define DISPLAY_HEIGHT 40
-
-// Output file is 5x5.bdf
 int main(int argc, char *argv[]) {
 
-//  system("cp header.txt 5x5.bdf");
-
-
-  //FILE *f = fopen("5x5.bdf", "w");
   FILE *f = stdout;
-  //  fprintf(f, "%02x", 32);
 
   for(int incr=0; incr < sizeof(glyph)/sizeof(*glyph);++incr){
     
     if (incr + ' ' >= 65) { // may need to add less than as well  (97(a) -33(!) = 64)
-      //do this twice
       fprintf(f, "STARTCHAR %c\n", ( incr+' '+32));
       fprintf(f, "ENCODING %d\n", ( incr+' '+32));
       fprintf(f, "SWIDTH 540 0\n");
@@ -47,14 +33,8 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < 5 ; i ++){
       fprintf(f, "%02x\n", glyph[incr][i] << 3);
     }
-    /*
-    //convert first 4 bits to hex, and second 4 bits to hex, then print.. continue for all 5 rows 
-    //glyph[incr] to hex
-    system(("echo '" + std::to_string(glyph[incr][0]) + "' >>5x5.bdf").c_str()); 
-     */
     fprintf(f, "ENDCHAR\n");
   }
-  //system("echo 'ENDFONT' >>5x5.bdf" );
   fprintf(f, "ENDFONT\n");
   fclose(f);
 }
