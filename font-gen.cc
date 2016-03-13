065278d1bcfb5a1e2abcc329a13095f70c3cc1d0
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-03-13 06:06:54 -0700

    Finishing touches

--- font-gen.cc
+++ font-gen.cc
@@ -22,32 +22,32 @@ const char START_CHAR=' ';
 
 int main(int argc, char *argv[]) {
 
-  FILE *f = stdout;
+    FILE *f = stdout;
 
-  for(int incr=0; incr < sizeof(glyph)/sizeof(*glyph);++incr){
+    for(int incr=0; incr < sizeof(glyph)/sizeof(*glyph);++incr){
 
-    // Uses same glyphs for lowercase as it does for upper
-    if (incr + START_CHAR >= 'A' && incr + START_CHAR <= 'Z') { 
-      write_character_data(f, incr + START_CHAR + 32, incr); // +32 for lowercase; 
+	// Uses same glyphs for lowercase as it does for upper
+	if (incr + START_CHAR >= 'A' && incr + START_CHAR <= 'Z') { 
+	    write_character_data(f, incr + START_CHAR + 32, incr); // +32 for lowercase; 
+	}
+	write_character_data(f, incr + START_CHAR, incr); 
     }
-    write_character_data(f, incr + START_CHAR, incr); 
-  }
 
-  fprintf(f, "ENDFONT\n");
-  fclose(f);
+    fprintf(f, "ENDFONT\n");
+    fclose(f);
 }
 
 void write_character_data(FILE *f, int ascii, int glyph_number){
-  fprintf(f, "STARTCHAR %c\n", (ascii));
-  fprintf(f, "ENCODING %d\n", (ascii));
-  fprintf(f, "SWIDTH 540 0\n");
-  fprintf(f, "DWIDTH 5 0\n");
-  fprintf(f, "BBX 5 5 0 -1\n");
-  fprintf(f, "BITMAP\n");
+    fprintf(f, "STARTCHAR %c\n", (ascii));
+    fprintf(f, "ENCODING %d\n", (ascii));
+    fprintf(f, "SWIDTH 540 0\n");
+    fprintf(f, "DWIDTH 5 0\n");
+    fprintf(f, "BBX 5 5 0 -1\n");
+    fprintf(f, "BITMAP\n");
 
-  for (int i = 0; i < 5 ; i ++){
-    fprintf(f, "%02x\n", glyph[glyph_number][i] << 3);
-  }
+    for (int i = 0; i < 5 ; i ++){
+	fprintf(f, "%02x\n", glyph[glyph_number][i] << 3);
+    }
 
-  fprintf(f, "ENDCHAR\n");
+    fprintf(f, "ENDCHAR\n");
 }
